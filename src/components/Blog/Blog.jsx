2fe8e8a96aa65b11b71735a89b8e4fb9eb840782
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-24 00:06:29 +0600

    added updated file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,17 @@
-
-const Blog = () => {
+// using propsTypes
+import PropTypes from 'prop-types';
+const Blog = ({ blog }) => {
+    console.log(blog);
     return (
         <div>
 
         </div>
     );
 };
+
+// using propsTypes
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
 
 export default Blog;
