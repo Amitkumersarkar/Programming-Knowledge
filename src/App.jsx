e61579fc97e4bccd6528caad5d71d713c61ec5d4
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-23 22:20:32 +0600

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>
-      <h3></h3>
+      <Header></Header>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className='text-3xl font-bold p-5'>Programming Knowledge</h3>
+
+        </div>
+    );
+};
+
+export default Header;
