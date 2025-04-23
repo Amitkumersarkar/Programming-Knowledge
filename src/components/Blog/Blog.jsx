453e0beb16d1c92096b5ba2acbd90586f001037a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-24 02:09:47 +0600

    added new updated file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,9 +2,11 @@
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
     console.log(blog);
+    const { cover, title, author, hashtags, image, postDate, readingTime, } = blog;
     return (
         <div>
-
+            <img className='w-96 h-96 p-4' src={cover} alt={`Cover picture of the title ${title}`} />
+            <h2 className='text-2xl'>Title : {title}</h2>
         </div>
     );
 };
